9267afd084f27a6503becd9b22d53d17809f4dd1
Sarah Yun <sarahyun7@gmail.com>
2018-11-02 20:13:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newRecipe = Object.assign({}, object)
+  delete newRecipe.key
+  return newRecipe
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
